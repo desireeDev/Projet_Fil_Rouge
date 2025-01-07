fac059d243e1cc6874be9a552cda4786ffe28afd
Desireedev <synticheattoh01@gmail.com>
2025-01-07 13:48:20 +0100

    Adaptation to format MVC

---
+++ src/main/java/com/example/bibliotheque/Controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.example.bibliotheque.Controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MenuController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
