ef0c6edb790c962c53da0b1d0e0a2e28519c68fb
Desireedev <synticheattoh01@gmail.com>
2025-01-14 11:12:57 +0100

    Integration of Part de Leo

--- src/main/java/com/example/bibliotheque/Controller/MenuController.java
+++ src/main/java/com/example/bibliotheque/Controller/MenuController.java
@@ -1,15 +1,40 @@
 package com.example.bibliotheque.Controller;
 //Les importations utiles
+import com.example.bibliotheque.Model.Bibliotheque;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+
+import java.awt.*;
+import java.io.File;
+
 public class MenuController {
     @FXML
-    private void handleOpen() {
+    private void handleOpen() throws JAXBException {
         // Code pour gérer l'action "Ouvrir"
-        System.out.println("Ouvrir!");
-    }
+        String filePath;
+        File xmlStream;
+        Bibliotheque b;
+        JAXBContext context = JAXBContext.newInstance(Bibliotheque.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        FileDialog fd = new FileDialog((java.awt.Frame)null, "Choose a file", FileDialog.LOAD);
+        fd.setFile("*.xml");
+        fd.setVisible(true);
+        String filename = fd.getFile();
+        if (filename == null) {
+            System.out.println("You cancelled the choice");
+            return;
+        }
+        else
+            System.out.println("You chose " + fd.getDirectory() + filename);
 
+        filePath = fd.getDirectory() + fd.getFile();
+        xmlStream = new File(filePath);
+        b =  (Bibliotheque) unmarshaller.unmarshal(xmlStream);
+    }
     @FXML
     private void handleLeft() {
         // Code pour gérer l'action "Quitter"
