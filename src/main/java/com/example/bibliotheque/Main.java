fa8d00549491c0abe3e210e7b4db17403be9dfe0
Desireedev <synticheattoh01@gmail.com>
2025-01-15 12:06:21 +0100

    Verifications

--- src/main/java/com/example/bibliotheque/Main.java
+++ src/main/java/com/example/bibliotheque/Main.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
 import java.io.IOException;
-
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -14,7 +12,6 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage)throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View/home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Ma Bibliothèque");
