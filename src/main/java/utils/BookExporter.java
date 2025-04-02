48200280872587af8a69c6ec19d9875a46fba4a4
Asmatrkk <atrouky94@gmail.com>
2025-04-02 15:14:07 +0200

    Mes nouvelles modifs depuis version clean

--- src/main/java/utils/BookExporter.java
+++ src/main/java/utils/BookExporter.java
@@ -1,22 +1,27 @@
 package utils;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.util.Units;
 import com.example.bibliotheque.Model.Livre;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 import java.math.BigInteger;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBookmark;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class BookExporter {
     public BookExporter() {
     }
+
     /**
      * Convertion
      * @param books
@@ -66,7 +71,40 @@ public class BookExporter {
 
     public void createCoverPage(XWPFDocument doc) {
 
+        // Grand titre centré
+        XWPFParagraph title = doc.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("Livre de la bibliothèque");
+        titleRun.setBold(true);
+        titleRun.setFontSize(28);
+        titleRun.addCarriageReturn();
+        titleRun.addBreak();
+
+        XWPFParagraph devParagraph = doc.createParagraph();
+        devParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun devRun = devParagraph.createRun();
+        devRun.setText("Développeurs : Asma, Syntiche, Isaac, Léo");
+        devRun.setFontSize(14);
+        devRun.setItalic(true);
+        devRun.addBreak();
+        devRun.addBreak();
+
+        try {
+            String imgFile = "src/main/java/utils/myimg.png";
+            FileInputStream is = new FileInputStream(imgFile);
+            titleRun.addPicture(is,
+                    Document.PICTURE_TYPE_JPEG,
+                    imgFile,
+                    Units.toEMU(400),  // largeur en EMUs
+                    Units.toEMU(300)); // hauteur en EMUs
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
     /**
      * Création d'une entête presente dans toutes les pages du fichier word
      * @param doc
@@ -135,7 +173,6 @@ public class BookExporter {
     public void createChapters(XWPFDocument doc, Livre[] livres) {
         for(int i = 0; i < livres.length; i++) {
             XWPFParagraph paragraph = doc.createParagraph();
-
             //Titre du livre
             XWPFRun run = paragraph.createRun();
             run.setText(livres[i].getTitre());
@@ -153,34 +190,96 @@ public class BookExporter {
             bookmark.setId(BigInteger.valueOf(i+1));
             ctp1.addNewBookmarkEnd().setId(BigInteger.valueOf(i+1));
 
-            // Ajouter du texte dans le chapitre
-            XWPFRun contentParagraph = paragraph.createRun();
-            String contenu  = "contenu";
-            contentParagraph.setText(contenu);
+            XWPFParagraph infoParagraph = doc.createParagraph();
+            XWPFRun infoRun = infoParagraph.createRun();
+            infoRun.setText("Auteur : " + livres[i].getAuteur());
+            infoRun.addBreak();
+            infoRun.setText("Parution : " + livres[i].getParution());
+            infoRun.addBreak();
+            infoRun.setText("Position : Rangée " + livres[i].getRangee()+ " Colonne " + livres[i].getColonne());
+            infoRun.addBreak();
+            infoRun.setText("Présentation : " + livres[i].getPresentation());
             paragraph.setPageBreak(true);
+
+            String imagePath = livres[i].getPathImage();
+            if (imagePath != null && !imagePath.isEmpty()) {
+                try {
+                    InputStream imageStream;
+                    boolean isUrl = imagePath.startsWith("http://") || imagePath.startsWith("https://");
+
+                    if (isUrl) {
+                        // Lien distant
+                        URL url = new URL(imagePath);
+                        imageStream = url.openStream();
+                    } else {
+                        // Chemin local
+                        File imageFile = new File(imagePath);
+                        if (!imageFile.exists()) {
+                            throw new IOException("Image non trouvée : " + imagePath);
+                        }
+                        imageStream = new FileInputStream(imageFile);
+                    }
+
+                    XWPFParagraph imageParagraph = doc.createParagraph();
+                    imageParagraph.setAlignment(ParagraphAlignment.CENTER);
+                    XWPFRun imageRun = imageParagraph.createRun();
+                    imageRun.addPicture(imageStream,
+                            Document.PICTURE_TYPE_JPEG,
+                            imagePath,
+                            Units.toEMU(250),
+                            Units.toEMU(380));
+                    imageStream.close();
+
+                }
+
+                catch (Exception e) {
+                    System.out.println("Erreur lors de l'ajout de l'image pour le livre : " + livres[i].getTitre());
+                    e.printStackTrace();
+
+                }
+            }
+
+
         }
     }
+
+
     public void createTableofUnavailableBooks (XWPFDocument doc, Livre[] livres) {
         int cols = 2;
-        int livresNondispos = 0;
+        int rows = livres.length;
 
-        //On compte le nombres de livres non dispo et on créé un tableau avec autant de lignes
-        for (int row=0; row< livres.length; row++) {
-            if(livres[row].isEmprunte()) {
-                livresNondispos++;
-            }
-        }
-        XWPFTable table = doc.createTable(livresNondispos, cols);
-        //setColumnWidth(table, 0, 200);
+        // Titre du tableau
+        XWPFParagraph tableTitle = doc.createParagraph();
+        tableTitle.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun titleRun = tableTitle.createRun();
+        titleRun.setText("Tableau des Livres");
+        titleRun.setBold(true);
+        titleRun.setFontSize(16);
+        titleRun.addBreak();
 
-        for (int row=0,j=0; row< livres.length; row++) {
-            if(livres[row].isEmprunte()) {
-                XWPFTableRow tableRow = table.getRow(j);
-                tableRow.getCell(0).setText(livres[row].getTitre());
-                j++;
-            }
+        // Création du tableau avec autant de lignes que de livres
+        XWPFTable table = doc.createTable(rows + 1, cols); // +1 pour la ligne d'en-tête
+
+        // Ligne d'en-tête
+        XWPFTableRow headerRow = table.getRow(0);
+        headerRow.getCell(0).setText("Titre");
+        headerRow.getCell(1).setText("Disponibilité");
+
+        // Lignes des livres
+        for (int i = 0; i < livres.length; i++) {
+            XWPFTableRow tableRow = table.getRow(i + 1); // +1 car la ligne 0 est l'en-tête
+
+            // Colonne 1 : Titre
+            tableRow.getCell(0).setText(livres[i].getTitre());
+
+            // Colonne 2 : Disponibilité
+            String dispo = livres[i].isEmprunte() ? "Indisponible" : "Disponible";
+            tableRow.getCell(1).setText(dispo);
         }
     }
+
+
+
     public  void setColumnWidth(XWPFTable table, int colIndex, int width) {
         CTTblWidth tblWidth = CTTblWidth.Factory.newInstance();
         //width : largeur
