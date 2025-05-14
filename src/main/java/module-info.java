2748d1a2c257db070e18190c1b2f0b91765ae9dc
SAD🤎👩‍💻 <114066560+desireeDev@users.noreply.github.com>
2025-05-14 17:50:20 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,13 +12,13 @@ module com.example.bibliotheque {
     requires com.almasb.fxgl.all;
     requires jakarta.xml.bind;
     requires java.desktop;
-    requires org.apache.poi.poi;
-    requires org.apache.poi.ooxml;
-    requires org.apache.commons.collections4;
-    requires org.apache.xmlbeans;
-    requires org.apache.poi.ooxml.schemas;
-    requires mysql.connector.j;
-    requires java.sql;
+    //requires org.apache.poi.poi;
+    //requires org.apache.poi.ooxml;
+    //requires org.apache.commons.collections4;
+    //requires org.apache.xmlbeans;
+    //requires org.apache.poi.ooxml.schemas;
+    //requires mysql.connector.j;
+    //requires java.sql;
 
     opens com.example.bibliotheque to javafx.fxml;
     exports com.example.bibliotheque;
